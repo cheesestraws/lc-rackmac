46a175c02854dda793d531114cf56be1a86ba030
cheesestraws <rob@midworld.co.uk>
2024-08-22 15:25:32 +0100

    rename project

---
+++ code/main/pins.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <driver/gpio.h>
+
+#define PIN_RESET        GPIO_NUM_32
+#define PIN_LT_ACT_LED   GPIO_NUM_33
+#define PIN_LT_ERR_LED   GPIO_NUM_5
+#define PIN_1WIRE        GPIO_NUM_17
+#define PIN_HDLED_IN     GPIO_NUM_39
+#define PIN_FANTACH      GPIO_NUM_36
+#define PIN_HDLED_OUT    GPIO_NUM_15
+#define PIN_TT_CTS       GPIO_NUM_14
+#define PIN_TT_TX        GPIO_NUM_12
+#define PIN_TT_RX        GPIO_NUM_4
+#define PIN_SNDOUT       GPIO_NUM_2
